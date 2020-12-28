eb64ff449df438fe7b1010ba272b20a0d97c75ea
sun912 <keunsun.lee@lge.com>
2020-12-28 10:32:17 +0900

    dwarf_height.py upload

---
+++ dwarf_height.py
@@ -0,0 +1,14 @@
+import sys
+n = 9
+height = [int(input()) for _ in range(n)]
+height.sort()
+total = sum(height)
+for i in range(0,n):
+    for j in range(i+1,n):
+        if total - height[i] - height[j] == 100:
+            for k in range(0,n):
+                if k == i or k == j:
+                    continue
+                print(height[k])
+
+            sys.exit(0)
